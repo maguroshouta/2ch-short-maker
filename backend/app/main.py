ed4cc0ddf868e97c50c71106801d182aa1ccbbe2
maguroshouta <maguroshouta@gmail.com>
2024-12-23 12:38:54 +0900

    cors対応

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from fastapi.concurrency import asynccontextmanager
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.main import api_router
 from app.core.db import create_db_and_tables
+from app.core.env import FRONTEND_URL
 
 
 @asynccontextmanager
@@ -12,5 +14,17 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+origins = [
+    FRONTEND_URL,
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_router, prefix="/api")

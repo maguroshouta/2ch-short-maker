5706929a260e735f369c7ffac8506d11cc614c5e
maguroshouta <maguroshouta@gmail.com>
2024-12-23 09:56:27 +0900

    first commit

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Noto_Sans_JP } from "next/font/google";
+import "./globals.css";
+
+const notoSansJP = Noto_Sans_JP({
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "2ch ショートメーカー",
+  description: "量産型のショートを生成します",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${notoSansJP.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="mt-32 flex flex-col items-center gap-2">
+      <h1 className="font-bold text-4xl">2ch ショートメーカー</h1>
+      <p className="text-xl text-">量産型のショートをAIが生成します</p>
+    </main>
+  );
+}

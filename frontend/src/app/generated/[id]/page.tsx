bcfaaf83a9516de61a2ba12562a0399896c3bf3d
maguroshouta <maguroshouta@gmail.com>
2024-12-25 20:52:31 +0900

    動画視聴ページ

--- frontend/src/app/generated/[id]/page.tsx
+++ frontend/src/app/generated/[id]/page.tsx
@@ -1,5 +1,43 @@
-export default async function GeneratePage({
+import VideoSwiper from "@/components/video-swiper";
+
+export default async function Page({
 	params,
-}: { params: { id: string } }) {
-	return <div>{params.id}</div>;
+}: {
+	params: Promise<{ id: string }>;
+}) {
+	const id = (await params).id;
+
+	const video_res = await fetch(
+		`${process.env.NEXT_PUBLIC_API_URL}/api/videos/info/${id}`,
+		{
+			cache: "no-cache",
+		},
+	);
+	const video: Video = await video_res.json();
+
+	const recent_res = await fetch(
+		`${process.env.NEXT_PUBLIC_API_URL}/api/videos/recent`,
+		{
+			cache: "no-cache",
+		},
+	);
+
+	const videos: Video[] = await recent_res.json();
+
+	return (
+		<div className="my-8 flex flex-col items-center gap-4">
+			<div className="flex flex-col gap-2">
+				<h1 className="text-4xl">テーマ: {video.prompt}</h1>
+				<video className="w-64 rounded-lg md:w-96" controls loop>
+					<source
+						src={`${process.env.NEXT_PUBLIC_API_URL}/api/videos/generated/${video.id}`}
+					/>
+				</video>
+			</div>
+			<h2 className="my-4 text-2xl font-bold">最近生成された動画</h2>
+			<div className="w-full">
+				<VideoSwiper videos={videos} />
+			</div>
+		</div>
+	);
 }

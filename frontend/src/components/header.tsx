c93f330d89311da916b8783c533c9c39410e9377
maguroshouta <maguroshouta@gmail.com>
2024-12-25 20:55:11 +0900

    ヘッダーのテキストクリックでホーム

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,9 +1,11 @@
-import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Header() {
 	return (
 		<header className="w-full p-4 border text-2xl flex justify-between items-center">
-			<h1 className="font-bold">2ch ショートメーカー</h1>
+			<Link href="/">
+				<h1 className="font-bold">2ch ショートメーカー</h1>
+			</Link>
 		</header>
 	);
 }

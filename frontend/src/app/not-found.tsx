ef6bc0c6b8371f558deadafe7575351cf18cbbcc
maguroshouta <maguroshouta@gmail.com>
2024-12-25 14:52:54 +0900

    404ページ

---
+++ frontend/src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+	return (
+		<main className="mt-16 w-full flex flex-col items-center gap-4">
+			<h1 className="text-4xl font-bold">404 Not Found</h1>
+			<p className="text-xl">ページが見つかりません</p>
+		</main>
+	);
+}

e8718b6c59431c3e713f941ca4f75d119dd0ccef
maguroshouta <maguroshouta@gmail.com>
2024-12-25 20:54:21 +0900

    最近生成された動画を見れるように

--- frontend/src/components/video-card.tsx
+++ frontend/src/components/video-card.tsx
@@ -1,14 +1,18 @@
+import Link from "next/link";
+
 export default function VideoCard({ video }: { video: Video }) {
 	return (
-		<div className="m-4 relative transition hover:-translate-y-4 cursor-pointer">
-			<img
-				className="w-64 rounded-xl shadow-md"
-				src={`${process.env.NEXT_PUBLIC_API_URL}/api/videos/generated/${video.id}/thumbnail`}
-				alt={video.prompt}
-			/>
-			<div className="flex absolute bottom-4 left-4 bg-white p-2 rounded-xl">
-				<p className="overflow-hidden font-bold">{video.prompt}</p>
+		<Link href={`/generated/${video.id}`}>
+			<div className="m-4 relative transition hover:scale-105 cursor-pointer">
+				<img
+					className="w-64 rounded-xl shadow-md"
+					src={`${process.env.NEXT_PUBLIC_API_URL}/api/videos/generated/${video.id}/thumbnail`}
+					alt={video.prompt}
+				/>
+				<div className="flex absolute bottom-4 left-4 bg-white p-2 rounded-xl">
+					<p className="overflow-hidden font-bold">{video.prompt}</p>
+				</div>
 			</div>
-		</div>
+		</Link>
 	);
 }

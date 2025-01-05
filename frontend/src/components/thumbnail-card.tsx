d1ee786a72e325a4ac96a5336edd2e64bea45273
maguroshouta <maguroshouta@gmail.com>
2025-01-05 16:17:23 +0900

    サムネサイズの調整

--- frontend/src/components/thumbnail-card.tsx
+++ frontend/src/components/thumbnail-card.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 export default function ThumbnailCard({ video }: { video: Video }) {
 	return (
 		<Link href={`/generated/${video.id}`}>
-			<div className="m-4 relative transition hover:scale-105 cursor-pointer">
+			<div className="m-4 transition hover:scale-105 cursor-pointer">
 				<img
-					className="w-48 rounded-xl shadow-md md:w-64"
+					className="w-64 rounded-xl shadow-md"
 					src={`${process.env.NEXT_PUBLIC_API_URL}/api/videos/${video.id}.jpg`}
 					alt={video.prompt}
 				/>

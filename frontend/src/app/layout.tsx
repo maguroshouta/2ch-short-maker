ffb89b0dee2ca625deace1b65df26f8b953eea5c
maguroshouta <maguroshouta@gmail.com>
2025-01-06 21:45:18 +0900

    ダークモード

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Noto_Sans_JP } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
+import { ThemeProvider } from "next-themes";
 
 const notoSansJP = Noto_Sans_JP({
 	subsets: ["latin"],
@@ -19,11 +20,18 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="ja">
+		<html lang="ja" suppressHydrationWarning>
 			<body className={`${notoSansJP.className} antialiased`}>
-				<Header />
-				{children}
-				<Toaster />
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="system"
+					enableSystem
+					disableTransitionOnChange
+				>
+					<Header />
+					{children}
+					<Toaster />
+				</ThemeProvider>
 			</body>
 		</html>
 	);
--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
+import { ThemeToggle } from "@/components/theme-toggle";
 
 export default function Header() {
 	return (
-		<header className="w-full p-4 border text-2xl flex justify-between items-center">
+		<header className="w-full p-4 border-b text-2xl flex justify-between items-center">
 			<Link href="/">
 				<h1 className="font-bold">2ch ショートメーカー</h1>
 			</Link>
+			<ThemeToggle />
 		</header>
 	);
 }
